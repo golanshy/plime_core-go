523edbaf8000045176166a621abf123276135e36
Golan Shay <golanshy@gmail.com>
2020-03-22 08:47:23 +0000

    refactored to rest_errors

--- src/utils/mysql_utils/mysql_utils.go
+++ src/utils/mysql_utils/mysql_utils.go
@@ -2,7 +2,7 @@ package mysql_utils
 
 import (
 	"github.com/go-sql-driver/mysql"
-	"github.com/golanshy/plime_core-go/src/utils/errors"
+	"github.com/golanshy/plime_core-go/src/utils/rest_errors"
 	"strings"
 )
 
@@ -10,17 +10,17 @@ const (
 	ErrorNoRows = "no rows in result set"
 )
 
-func ParseError(err error) *errors.RestErr {
+func ParseError(err error) *rest_errors.RestErr {
 	sqErr, ok := err.(*mysql.MySQLError)
 	if !ok {
 		if strings.Contains(err.Error(), ErrorNoRows) {
-			return errors.NewNotFoundError("No record matching given id")
+			return rest_errors.NewNotFoundError("No record matching given id")
 		}
-		return errors.NewInternalServerError("error parsing database response", err)
+		return rest_errors.NewInternalServerError("error parsing database response", err)
 	}
 	switch sqErr.Number {
 	case 1062:
-		return errors.NewBadRequestError("duplicate field")
+		return rest_errors.NewBadRequestError("duplicate field")
 	}
-	return errors.NewInternalServerError("error processing request", err)
+	return rest_errors.NewInternalServerError("error processing request", err)
 }
