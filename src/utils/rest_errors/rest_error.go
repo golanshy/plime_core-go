95548b2a2fe266a920973f3dad9b829d177bbab3
Golan Shay <golanshy@gmail.com>
2020-03-24 11:56:08 +0000

    errors fixed

--- src/utils/rest_errors/rest_error.go
+++ src/utils/rest_errors/rest_error.go
@@ -6,11 +6,11 @@ import (
 	"net/http"
 )
 
-type RestErr interface {
-	Message() string
-	Status() int
-	Error() string
-	Causes() []interface{}
+type RestErr struct {
+	Message string        `json:"message"`
+	Status  int           `json:"status"`
+	Error   string        `json:"error"`
+	Causes  []interface{} `json:"causes"`
 }
 
 func NewRestErrorFromBytes(bytes []byte) (*RestErr, error) {
@@ -27,46 +27,45 @@ func NewRestError(message string, status int, error string) *RestErr {
 		Status:  status,
 		Error:   error,
 	}
-	return apiError, nil
 }
 
-func NewBadRequestError(message string) RestErr {
-	return &restErr{
-		message: message,
-		status:  http.StatusBadRequest,
-		error:   "bad_request",
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
 	}
 }
 
-func NewNotFoundError(message string) RestErr {
-	return &restErr{
-		message: message,
-		status:  http.StatusNotFound,
-		error:   "not_found",
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
 	}
 }
 
-func NewInternalServerError(message string, err error) RestErr {
-	return &restErr{
-		message: message,
-		status:  http.StatusInternalServerError,
-		error:   "internal_server_error",
-		causes:  []interface{}{err},
+func NewInternalServerError(message string, err error) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
+		Causes:  []interface{}{err},
 	}
 }
 
-func NewUnauthorizedError(message string) RestErr {
-	return &restErr{
-		message: message,
-		status:  http.StatusUnauthorized,
-		error:   "unauthorized_access",
+func NewUnauthorizedError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusUnauthorized,
+		Error:   "unauthorized_access",
 	}
 }
 
-func NewForbiddenEmailVerificationError(message string) RestErr {
-	return &restErr{
-		message: message,
-		status:  http.StatusForbidden,
-		error:   "forbidden_access",
+func NewForbiddenEmailVerificationError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusForbidden,
+		Error:   "forbidden_access",
 	}
 }
