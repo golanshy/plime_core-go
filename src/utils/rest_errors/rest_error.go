8be2e7eb604e411be173b2855d08113e84930472
Golan Shay <golanshy@gmail.com>
2020-03-24 11:15:37 +0000

    WIP

--- src/utils/rest_errors/rest_error.go
+++ src/utils/rest_errors/rest_error.go
@@ -21,7 +21,7 @@ type restErr struct {
 	causes  []interface{} `json:"causes"`
 }
 
-func (e restErr) restErr() string {
+func (e restErr) RestErr() restErr {
 	return fmt.Sprintf("message: %s - status %d - error: %s - causes: [ %v]", e.message, e.status, e.error, e.causes)
 }
 
@@ -42,8 +42,8 @@ func NewError(message string) error {
 	return errors.New(message)
 }
 
-func NewRestError(message string, status int, error string, causes []interface{}) RestErr {
-	return &restErr{
+func NewRestError(message string, status int, error string, causes []interface{}) restErr {
+	return restErr{
 		message: message,
 		status:  status,
 		error:   error,
@@ -60,7 +60,7 @@ func NewRestErrorFromBytes(bytes []byte) (RestErr, error) {
 }
 
 func NewBadRequestError(message string) RestErr {
-	return restErr{
+	return &restErr{
 		message: message,
 		status:  http.StatusBadRequest,
 		error:   "bad_request",
@@ -68,7 +68,7 @@ func NewBadRequestError(message string) RestErr {
 }
 
 func NewNotFoundError(message string) RestErr {
-	return restErr{
+	return &restErr{
 		message: message,
 		status:  http.StatusNotFound,
 		error:   "not_found",
@@ -76,7 +76,7 @@ func NewNotFoundError(message string) RestErr {
 }
 
 func NewInternalServerError(message string, err error) RestErr {
-	return restErr{
+	return &restErr{
 		message: message,
 		status:  http.StatusInternalServerError,
 		error:   "internal_server_error",
@@ -85,7 +85,7 @@ func NewInternalServerError(message string, err error) RestErr {
 }
 
 func NewUnauthorizedError(message string) RestErr {
-	return restErr{
+	return &restErr{
 		message: message,
 		status:  http.StatusUnauthorized,
 		error:   "unauthorized_access",
@@ -93,7 +93,7 @@ func NewUnauthorizedError(message string) RestErr {
 }
 
 func NewForbiddenEmailVerificationError(message string) RestErr {
-	return restErr{
+	return &restErr{
 		message: message,
 		status:  http.StatusForbidden,
 		error:   "forbidden_access",
---
+++ src/utils/rest_errors/rest_error_test.go
@@ -0,0 +1,64 @@
+package rest_errors
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestNewInternalServerError(t *testing.T) {
+	err := NewInternalServerError("this is the message", errors.New("this is the error"))
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status())
+	assert.EqualValues(t, "this is the message", err.Message())
+	assert.EqualValues(t, "internal_server_error", err.Error())
+
+	assert.NotNil(t, err.Causes())
+	assert.EqualValues(t, 1, len(err.Causes()))
+	//assert.EqualValues(t, "this is the error", err.Causes()[0])
+}
+
+func TestNewBadRequestError(t *testing.T) {
+	// TODO: Test!!!
+}
+
+func TestNewError(t *testing.T) {
+	// TODO: Test!!!
+}
+
+func TestNewForbiddenEmailVerificationError(t *testing.T) {
+	// TODO: Test!!!
+}
+
+func TestNewNotFoundError(t *testing.T) {
+	// TODO: Test!!!
+}
+
+func TestNewRestError(t *testing.T) {
+	// TODO: Test!!!
+}
+
+func TestNewRestErrorFromBytes(t *testing.T) {
+	// TODO: Test!!!
+}
+
+func TestNewUnauthorizedError(t *testing.T) {
+	// TODO: Test!!!
+}
+
+func TestRestErr_Causes(t *testing.T) {
+	// TODO: Test!!!
+}
+
+func TestRestErr_Error(t *testing.T) {
+	// TODO: Test!!!
+}
+
+func TestRestErr_Message(t *testing.T) {
+	// TODO: Test!!!
+}
+
+func TestRestErr_Status(t *testing.T) {
+	// TODO: Test!!!
+}
