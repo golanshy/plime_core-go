cd21d19c8e0f5d16cceb453999b51af5e4f40898
Golan Shay <golanshy@gmail.com>
2020-06-02 15:24:34 +0100

    kyc data modules

---
+++ data_models/kyc_dto/KycStatus.go
@@ -0,0 +1,63 @@
+package kyc_dto
+
+const (
+	KycNotStarted                   string = "kyc_status_not_started"
+	KycStatusInProgress             string = "kyc_status_in_progress"
+	KycStatusInReview               string = "kyc_status_in_review"
+	KycStatusAdditionalDataRequired string = "kyc_status_additional_data_required"
+	KycStatusApproved               string = "kyc_status_kyc_status_approved"
+	KycStatusDeclined               string = "kyc_status_declined"
+	KycStatusBanned                 string = "kyc_status_banned"
+
+	KycLevel0 int = 0
+	KycLevel1 int = 1
+	KycLevel2 int = 2
+	KycLevel3 int = 3
+	KycLevel4 int = 4
+	KycLevel5 int = 5
+
+	DocumentTypePassport       string = "document_type_passport"
+	DocumentTypeDrivingLicence string = "document_type_driving_licence"
+	DocumentTypePhotoId        string = "document_type_photo_id"
+	DocumentTypeSelfie         string = "document_type_selfie"
+	DocumentTypeSelfieVideo    string = "document_type_selfie_video"
+	DocumentTypeProofOfAddress string = "document_type_proof_of_address"
+
+	DocumentStatusRequired string = "document_status_required"
+	DocumentStatusSupplied string = "document_status_supplied"
+	DocumentStatusInReview string = "document_status_in_review"
+	DocumentStatusApproved string = "document_status_approved"
+	DocumentStatusRejected string = "document_status_rejected"
+)
+
+type KycStatus struct {
+	Status        string        `json:"status"`
+	LevelApproved int           `json:"level_approved"`
+	Documents     []KycDocument `json:"documents"`
+}
+
+type KycDocument struct {
+	Name     string   `json:"name"`
+	Quantity int      `json:"quantity"`
+	Types    []string `json:"types"`
+	Status   string   `json:"status"`
+	Required bool     `json:"required"`
+	Details  string   `json:"details"`
+}
+
+func NewKycStatus() *KycStatus {
+	return &KycStatus{
+		Status:        KycNotStarted,
+		LevelApproved: KycLevel0,
+		Documents:     make([]KycDocument, 0),
+	}
+}
+
+func NewKycDocument() *KycDocument {
+	return &KycDocument{
+		Quantity: 1,
+		Status:   DocumentStatusRequired,
+		Required: true,
+		Types:    make([]string, 0),
+	}
+}
