9667099a5a25c8c68b8f3da8223d0ce375467d2d
Golan Shay <golanshy@gmail.com>
2020-07-15 17:27:45 +0100

    notifications models

---
+++ data_models/notifications_dto/notifications_dto.go
@@ -0,0 +1,36 @@
+package notifications_dto
+
+import (
+	"github.com/golanshy/plime_core-go/utils/rest_errors"
+	"strings"
+)
+
+type NotificationRequest struct {
+	Channel     string  `json:"channel"`
+	UserIds     []int64 `json:"user_ids"`
+	Subject     string  `json:"subject"`
+	HtmlMessage string  `json:"html_message"`
+	Message     string  `json:"message"`
+	Payload     string  `json:"payload"`
+	DateCreated string  `json:"date_created"`
+}
+
+const (
+	ChannelEmail    string = "email"
+	ChannelText     string = "text"
+	ChannelWhatsApp string = "whatsapp"
+	ChannelPush     string = "push"
+)
+
+func (r *NotificationRequest) Validate() *rest_errors.RestErr {
+	if len(r.UserIds) == 0 {
+		return rest_errors.NewBadRequestError("missing recipients")
+	}
+	if strings.TrimSpace(r.Message) == "" && strings.TrimSpace(r.Channel) == "" {
+		return rest_errors.NewBadRequestError("missing channel")
+	}
+	if strings.TrimSpace(r.Message) == "" && strings.TrimSpace(r.Payload) == "" {
+		return rest_errors.NewBadRequestError("missing message or payload")
+	}
+	return nil
+}
---
+++ data_models/user_device_dto/user_device.go
@@ -0,0 +1,29 @@
+package user_device_dto
+
+import (
+	"github.com/golanshy/plime_core-go/utils/rest_errors"
+	"strings"
+)
+
+type UserDevice struct {
+	Platform  string `json:"platform"`
+	Make      string `json:"make"`
+	Model     string `json:"model"`
+	OsVersion string `json:"os_version"`
+	AppName   string `json:"app_name"`
+	PushId    string `json:"push_id"`
+	UserId    int64  `json:"user_id"`
+}
+
+func (d *UserDevice) Validate() *rest_errors.RestErr {
+	if strings.TrimSpace(d.PushId) == "" {
+		return rest_errors.NewBadRequestError("missing device push id")
+	}
+	return nil
+}
+
+type UserDevices struct {
+	Results []UserDevice `json:"user_devices"`
+}
+
+
