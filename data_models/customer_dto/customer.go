41fc120ebcafa7dfc59c2d28481e60d1e92e2017
Golan Shay <golanshy@gmail.com>
2020-10-31 12:03:41 +0000

    customer request data models

--- data_models/customer_dto/customer.go
+++ data_models/customer_dto/customer.go
@@ -8,6 +8,36 @@ import (
 	"strings"
 )
 
+type CustomerRequest struct {
+	Name                  string              `json:"name,omitempty"`
+	Details               string              `json:"details,omitempty"`
+	CompanyRegisteredName string              `json:"company_registered_name,omitempty"`
+	CompanyRegisteredId   string              `json:"company_registered_id,omitempty"`
+	Address               address_dto.Address `json:"address,omitempty"`
+}
+
+func (customerRequest *CustomerRequest) Trim() {
+	customerRequest.Name = strings.TrimSpace(customerRequest.Name)
+	customerRequest.Details = strings.TrimSpace(customerRequest.Details)
+}
+
+func (customerRequest *CustomerRequest) Validate() *rest_errors.RestErr {
+	customerRequest.Trim()
+	if customerRequest.Name == "" {
+		return rest_errors.NewBadRequestError("invalid name field")
+	}
+	if customerRequest.CompanyRegisteredId == "" {
+		return rest_errors.NewBadRequestError("invalid company id field")
+	}
+	if customerRequest.Details == "" {
+		return rest_errors.NewBadRequestError("invalid details field")
+	}
+	if err := customerRequest.Address.Validate(); err != nil {
+		return rest_errors.NewBadRequestError(fmt.Sprintf("invalid customer address details - %s", err.Message))
+	}
+	return nil
+}
+
 type Customer struct {
 	Id                    string              `json:"id"`
 	Name                  string              `json:"name,omitempty"`
@@ -17,36 +47,11 @@ type Customer struct {
 	ContactPerson         user_dto.User       `json:"contact_person,omitempty"`
 	Status                string              `json:"status,omitempty"`
 	Address               address_dto.Address `json:"address,omitempty"`
+	CustomerUsers         []CustomerUser      `json:"customer_users,omitempty"`
 	DateCreated           string              `json:"date_created,omitempty"`
 }
 
-func (customer *Customer) Trim() {
-	customer.Name = strings.TrimSpace(customer.Name)
-	customer.Details = strings.TrimSpace(customer.Details)
-}
-
-func (customer *Customer) Validate() *rest_errors.RestErr {
-	customer.Trim()
-	if customer.Name == "" {
-		return rest_errors.NewBadRequestError("invalid name field")
-	}
-	if customer.CompanyRegisteredId == "" {
-		return rest_errors.NewBadRequestError("invalid company id field")
-	}
-	if customer.Details == "" {
-		return rest_errors.NewBadRequestError("invalid details field")
-	}
-	if customer.ContactPerson.FirstName == "" {
-		return rest_errors.NewBadRequestError("invalid contact person first name field")
-	}
-	if customer.ContactPerson.LastName == "" {
-		return rest_errors.NewBadRequestError("invalid contact person last name field")
-	}
-	if customer.ContactPerson.Email == "" {
-		return rest_errors.NewBadRequestError("invalid contact person field")
-	}
-	if err := customer.Address.Validate(); err != nil {
-		return rest_errors.NewBadRequestError(fmt.Sprintf("invalid customer address details - %s", err.Message))
-	}
-	return nil
+type CustomerUser struct {
+	User user_dto.User `json:"user,omitempty"`
+	Role string        `json:"role,omitempty"`
 }
--- data_models/role_dto/role.go
+++ data_models/role_dto/role.go
@@ -1,15 +1,11 @@
 package role_dto
 
 const (
-	PlimeAdmin     string = "plime_admin"
-	PlimeUser      string = "plime_user"
-	PlimeModerator string = "plime_moderator"
-	PlimeViewer    string = "plime_viewer"
-	Admin          string = "admin"
-	Moderator      string = "moderator"
-	Viewer         string = "viewer"
+	PlimeAdmin        string = "plime_admin"
+	PlimeUser         string = "plime_user"
+	PlimeModerator    string = "plime_moderator"
+	PlimeViewer       string = "plime_viewer"
+	CustomerAdmin     string = "customer_admin"
+	CustomerModerator string = "customer_moderator"
+	CustomerViewer    string = "customer_viewer"
 )
-
-type Role struct {
-	Name string `json:"name,omitempty"`
-}
