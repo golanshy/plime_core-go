98fa6c180abaeedf0ae29f21a19a03413796cb68
Golan Shay <golanshy@gmail.com>
2020-11-22 13:48:40 +0000

    walle id: string

--- data_models/wallet_dao/wallet.go
+++ data_models/wallet_dao/wallet.go
@@ -9,31 +9,32 @@ import (
 )
 
 type Wallet struct {
-	Id               primitive.ObjectID `json:"id,omitempty" bson:"_id, omitempty"`
-	HolderId         string             `json:"holder_id"`
-	PartnerRef       string             `json:"partner_ref,omitempty"`
-	PartnerProduct   string             `json:"partner_product,omitempty"`
-	CurrencyCode     string             `json:"currency_code"` // Iso 4217 https://en.wikipedia.org/wiki/ISO_4217
-	Iban             string             `json:"iban,omitempty"`
-	BicSwift         string             `json:"bic_swift,omitempty"`
-	WalletStatus     string             `json:"status"`
-	IbanStatus       string             `json:"iban_status,omitempty"`
-	Amount           float64            `json:"amount"`
-	CreatedAt        time.Time          `json:"created_at,omitempty"`
-	CountryCode      string             `json:"country_code,omitempty"`
-	UkSortCode       string             `json:"uk_sort_code,omitempty"`
-	UkAccountNumber  string             `json:"uk_account_number,omitempty"`
-	RestrictedWallet *RestrictedWallet  `json:"restricted_wallet,omitempty"`
+	Id               string            `json:"id"`
+	BankingId        string            `json:"banking_id"`
+	HolderId         string            `json:"holder_id"`
+	PartnerRef       string            `json:"partner_ref,omitempty"`
+	PartnerProduct   string            `json:"partner_product,omitempty"`
+	CurrencyCode     string            `json:"currency_code"` // Iso 4217 https://en.wikipedia.org/wiki/ISO_4217
+	Iban             string            `json:"iban,omitempty"`
+	BicSwift         string            `json:"bic_swift,omitempty"`
+	WalletStatus     string            `json:"status"`
+	IbanStatus       string            `json:"iban_status,omitempty"`
+	Amount           float64           `json:"amount"`
+	CreatedAt        time.Time         `json:"created_at,omitempty"`
+	CountryCode      string            `json:"country_code,omitempty"`
+	UkSortCode       string            `json:"uk_sort_code,omitempty"`
+	UkAccountNumber  string            `json:"uk_account_number,omitempty"`
+	RestrictedWallet *RestrictedWallet `json:"restricted_wallet,omitempty"`
 }
 
 func (wallet *Wallet) Validate() *rest_errors.RestErr {
-	if wallet.Id.Hex() == "" {
+	if strings.TrimSpace(wallet.Id) == "" {
 		return rest_errors.NewBadRequestError("invalid wallet id")
 	}
-	if wallet.HolderId == "" {
+	if strings.TrimSpace(wallet.HolderId) == "" {
 		return rest_errors.NewBadRequestError("invalid wallet holder id")
 	}
-	if wallet.CurrencyCode == "" {
+	if strings.TrimSpace(wallet.CurrencyCode) == "" {
 		return rest_errors.NewBadRequestError("invalid wallet currency code")
 	}
 	return nil
