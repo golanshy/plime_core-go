b5185a27a003612a9ce955a612ba362091af5825
Golan Shay <golanshy@gmail.com>
2020-12-22 10:56:59 +0000

    remove customer request

--- data_models/customer_dto/customer.go
+++ data_models/customer_dto/customer.go
@@ -13,47 +13,6 @@ import (
 //type SoleTraderAccount = 1
 //type BusinessAccount = 2
 
-type CustomerRequest struct {
-	Type                  int64               `json:"type"`
-	Name                  string              `json:"name,omitempty"`
-	Details               string              `json:"details,omitempty"`
-	CompanyRegisteredName string              `json:"company_registered_name,omitempty"`
-	CompanyRegisteredId   string              `json:"company_registered_id,omitempty"`
-	Address               address_dto.Address `json:"address,omitempty"`
-}
-
-func (customerRequest *CustomerRequest) Trim() {
-	customerRequest.Name = strings.TrimSpace(customerRequest.Name)
-	customerRequest.Details = strings.TrimSpace(customerRequest.Details)
-	customerRequest.CompanyRegisteredName = strings.TrimSpace(customerRequest.CompanyRegisteredName)
-	customerRequest.CompanyRegisteredId = strings.TrimSpace(customerRequest.CompanyRegisteredId)
-	customerRequest.Address.Trim()
-}
-
-func (customerRequest *CustomerRequest) Validate() *rest_errors.RestErr {
-	customerRequest.Trim()
-	if customerRequest.Type < 0 || customerRequest.Type > 2 {
-		return rest_errors.NewBadRequestError("invalid type field")
-	}
-	if customerRequest.Name == "" {
-		return rest_errors.NewBadRequestError("invalid name field")
-	}
-	if customerRequest.Type == 2 {
-		// Mandatory for Business Customers
-		if customerRequest.CompanyRegisteredName == "" {
-			return rest_errors.NewBadRequestError("invalid company name field")
-		}
-		if customerRequest.CompanyRegisteredId == "" {
-			return rest_errors.NewBadRequestError("invalid company id field")
-		}
-	}
-
-	if err := customerRequest.Address.Validate(); err != nil {
-		return rest_errors.NewBadRequestError(fmt.Sprintf("invalid customer address details - %s", err.Message))
-	}
-	return nil
-}
-
 type CustomersResult struct {
 	Start   int64      `json:"start"`
 	Limit   int64      `json:"limit"`
